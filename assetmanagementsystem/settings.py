38c502284eb744f72f8d833926a849ee7eb1979f
Rajan <stharj001@gmail.com>
2025-05-22 16:18:49 +0545

    cors

--- assetmanagementsystem/settings.py
+++ assetmanagementsystem/settings.py
@@ -203,14 +203,14 @@ SIMPLE_JWT = {
 
 PASSWORD_RESRT_TIMMEOUT=900
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000",
-#     "http://localhost:5173",
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
     
     
-# ]
-CORS_ALLOW_ALL_ORIGINS = True
+]
+# CORS_ALLOW_ALL_ORIGINS = True
 
 
 AUTHENTICATION_BACKENDS = [
